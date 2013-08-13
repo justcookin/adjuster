310192130724a354d86d8f8c2b760e842661484e
Justin Cook <jcook713@gmail.com>
2013-08-13 13:10:20 -0400

    Cleaned up the code a bit, indents and removed some comments from Adjuster.pm.

--- Adjuster/lib/Adjuster.pm
+++ Adjuster/lib/Adjuster.pm
@@ -4,28 +4,29 @@ use warnings;
 use Dancer;
 use Text::Aspell;
 use File::Basename;
-#use LWP::Simple qw( getprint );
 use Image::Magick;
 
 set serializer => 'XML';
 #set serializer => 'Dumper'; #un-comment this for json format responses
 
-#
 # adjust_image - takes height width and image url
 get '/adjust-image/:height/:width' => sub {
     my $url = params->{url};
     my $height = params->{height};
     my $width = params->{width};
-    # validate that height and width are numeric 
+
+    # validate that height and width are numeric  
     # limit to 4 places long, change if needed
     die "Height must be a numeric value\n" unless
 	$height =~ /^\d{1,4}$/;
     die "Width must be a numeric value\n" unless
 	$width =~ /^\d{1,4}$/;
+
     # resize image
     my $image = Image::Magick->new();
     my $x = $image->Read($url);
     $image->Resize(height => $height, width => $width);
+
     # save image
     my $name = basename($url);
     $x = $image->Write("/vagrant/Adjuster/public/images/$name");
@@ -35,8 +36,12 @@ get '/adjust-image/:height/:width' => sub {
 
 get '/spellcheck/:word' => sub {
     my $word = params->{word};
+    
+    # set up dictionary
     my $speller = Text::Aspell->new;
     $speller->set_option('sug-mode','fast');
+    
+    # check word and get suggestions if misspelled
     if ($speller->check( $word )) {
 	{  { SpellingCorrect => "true" ,
 	     Word => $word, }
